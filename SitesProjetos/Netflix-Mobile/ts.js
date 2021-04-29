fdb7881e8aefc86f43931591973abd6ea878a6bb
johnny <joaopaolinelli@gmail.com>
2021-04-29 20:40:21 -0300

    Web - Netflix mobile

---
+++ SitesProjetos/Netflix-Mobile/ts.js
@@ -0,0 +1,13 @@
+var valoresPositivos = 0;
+var stop = 0;
+var entrada = prompt( entrada);
+var entrada2 = ReadableStream;
+
+while(stop < 6) {
+  
+  if ( entrada > 0 && entrada != null) {
+    valoresPositivos = valoresPositivos +1;
+  }
+  stop++;
+}
+console.log(valoresPositivos + " valoresPositivos");
